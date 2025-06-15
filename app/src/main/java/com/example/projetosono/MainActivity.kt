2f0ec7a8db37b05128ccb9a0d66523d25e4ef972
rafatosto <rafanftosto@outlook.com>
2025-06-15 13:09:45 -0300

    Fazendo telas

--- app/src/main/java/com/example/projetosono/MainActivity.kt
+++ app/src/main/java/com/example/projetosono/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.projetosono
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var button: Button = findViewById(R.id.botao_cadastrar)
+
+        button.setOnClickListener {
+            val intent = Intent(this, Cadastrar::class.java)
+            startActivity(intent)
         }
     }
 }
